a1b2413a67e93eb10876565c514f53e8146bd990
Yagoopessoa <Yago-Pessoa@outlook.com>
2025-04-12 18:50:46 -0300

    Adicionando os nomes dos alunos na página index

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const alunos = [
-    { nome: "Maria Joaquinha", notas: [8, 7.5, 9], curso: "Sistemas para Internet" },
-    { nome: "João Ricardo", notas: [8, 8.5, 5], curso: "Direito" },
-    { nome: "José Henrique", notas: [4, 10, 7], curso: "Administração" },
-    { nome: "Pedro da Silva", notas: [6, 7.6, 7.5], curso: "Sistemas para Internet" },
-    { nome: "Silvana Morais", notas: [6, 7.5, 9.5], curso: "Sistemas de Informação" },
-    { nome: "Patricia Castro", notas: [1, 9, 10], curso: "Arquitetura" },
-    { nome: "Joana Ribeiro", notas: [8, 9, 9], curso: "Contabilidade" },
-    { nome: "Rafael Rocha", notas: [4, 4, 9], curso: "Sistemas para Internet" },
-    { nome: "Gustavo Henrique", notas: [8, 7.5, 5], curso: "Sistemas para Internet" }
-];
+const jsonString = '[{"nome": "Maria Joaquinha", "notas": [8,7.5,9], "curso": "Sistemas para Internet"}, {"nome": "João Ricardo", "notas": [8,8.5,5], "curso": "Direito"}, {"nome": "José Henrique", "notas": [4,10,7], "curso": "Administração"}, {"nome": "Pedro da Silva", "notas": [6,7.6,7.5], "curso": "Sistemas para Internet"}, {"nome": "Silvana Morais", "notas": [6,7.5,9.5], "curso": "Sistemas de Informação"}, {"nome": "Patricia Castro", "notas": [1,9,10], "curso": "Arquitetura"}, {"nome": "Joana Ribeiro", "notas": [8,9,9], "curso": "Contabilidade"}, {"nome": "Rafael Rocha", "notas": [4,4,9], "curso": "Sistemas para Internet"}, {"nome": "Gustavo Henrique", "notas": [8,7.5,5], "curso": "Sistemas para Internet"}]';
+
+
+const alunos = JSON.parse(jsonString);
+
 
 function imprimirMediaAprovados(alunos) {
     let output = ""; 
