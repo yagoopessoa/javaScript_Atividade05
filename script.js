ea06603791ec51ca1377cd48befbf045218b9d5c
Yagoopessoa <Yago-Pessoa@outlook.com>
2025-04-12 17:38:38 -0300

    Exercício 05

---
+++ script.js
@@ -0,0 +1,24 @@
+const alunos = [
+    { nome: "Maria Joaquinha", notas: [8, 7.5, 9], curso: "Sistemas para Internet" },
+    { nome: "João Ricardo", notas: [8, 8.5, 5], curso: "Direito" },
+    { nome: "José Henrique", notas: [4, 10, 7], curso: "Administração" },
+    { nome: "Pedro da Silva", notas: [6, 7.6, 7.5], curso: "Sistemas para Internet" },
+    { nome: "Silvana Morais", notas: [6, 7.5, 9.5], curso: "Sistemas de Informação" },
+    { nome: "Patricia Castro", notas: [1, 9, 10], curso: "Arquitetura" },
+    { nome: "Joana Ribeiro", notas: [8, 9, 9], curso: "Contabilidade" },
+    { nome: "Rafael Rocha", notas: [4, 4, 9], curso: "Sistemas para Internet" },
+    { nome: "Gustavo Henrique", notas: [8, 7.5, 5], curso: "Sistemas para Internet" }
+];
+
+function imprimirMediaAprovados(alunos) {
+    let output = ""; 
+    alunos.forEach(aluno => {
+        const media = aluno.notas.reduce((acc, nota) => acc + nota, 0) / aluno.notas.length;
+        if (media >= 7) {
+            output += `Nome: ${aluno.nome}, Média: ${media.toFixed(2)}, Curso: ${aluno.curso}\n`;
+        }
+    });
+    document.getElementById("output").innerText = output; 
+}
+
+imprimirMediaAprovados(alunos);
